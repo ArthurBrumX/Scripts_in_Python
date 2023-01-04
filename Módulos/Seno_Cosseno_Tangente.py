f972e8f286083348ee4c43dc7ec93c79efd25ff5
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-01-04 20:29:14 -0300

    modulo

--- Módulos/Seno_Cosseno_Tangente.py
+++ Módulos/Seno_Cosseno_Tangente.py
@@ -1,13 +1,20 @@
 # Scripts Que lê Um Ângulo Qualquer e Mostre Na Tela o Valor Do Seno, Cosseno e Tangente Desse Angulo.
 
 import math
+# Importando a biblioteca math (matematica)
 
 angulo = float(input("Digite o Ângulo Que Você Deseja: "))
+    # Vartiavel = angulo
+        # Mensagem = Digite o Angulo Que Voce Deseja:
 
 seno = math.sin(math.radians(angulo))
 
-print ("O Ângulo de {} Tem o SENO de {}".format(angulo, seno))
+print ("O dÂngulo de {} Tem o SENO de {}".format(angulo, seno))
 
 cosseno = math.cos(math.radians(angulo))
 
-print ("O ângulo De {} tem o COSSENO de {}")
+print ("O ângulo De {} tem o COSSENO de {}".format(angulo, cosseno))
+
+tangente = math.tan(math.radians(angulo))
+
+print ("O angulo de {} tem a Tangente de {}".format(angulo, tangente))
---
+++ Módulos/Sorteando_Um_Item_Na_Lista.py
@@ -0,0 +1,38 @@
+#Script Para Sortear Um Nome Digitado Pelo Usuario 
+
+print ("=" * 40)
+
+print ("Sorteio!")
+print ("Olá Usuário!")
+print ("Bem Vindo, Vamos Realizar Um Sorteio De Nomes! ")
+    # Boas Vindas Ao Usuário!.
+
+print ("=" * 40)
+
+import random
+    # Solicitando a Biblioteca (random).
+        # Ou ( From random import choice )
+
+pessoa_1 = input("Digite O Primero Nome:")
+    # Solicitando Um Dado Ao Usuário!
+
+pessoa_2 = input("Digite o Segundo Nome: ")
+    # Solicitando Um Dado Ao Usuário!
+
+pessoa_3 = input("Digite o Terceiro Nome: ")
+    # Solicitando Um Dado Ao Usuário!
+
+pessoa_4 = input ("Digite o Quarto Nome: ")
+    # Solicitando Um Dado Ao Usuário!
+
+sorteio = [pessoa_1, pessoa_2, pessoa_3, pessoa_4]
+    # Variável = Sorteio
+        # pessoas Que Irao Participar Do Sorteio.
+
+escolhindo = random.choice(sorteio)
+    # Solicitando a Funcao (Choice) Da Biblioteca (Random).
+        # Se for Declarar Que só irá usar a Função (Choice)
+            # Basta Apenas Adcionar (escolhido = choice(sorteio))
+
+print ("Parabens {}, voçê Foi o Sorteado!".format(escolhindo))
+    #Imprimindo Na Tela!
