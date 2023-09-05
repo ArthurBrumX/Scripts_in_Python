d7c5234d50a3c66d728430f700e71135da6eeb0b
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-09-05 11:24:11 -0300

    up

--- Banco SQLITE/Banco.py
+++ Banco SQLITE/Banco.py
@@ -8,4 +8,9 @@ banco = sqlite3.connect('produto275.db') # coloca o nome do banco de dados
 
 cursor = banco.cursor()
 
-cursor.execute("CREATE TABLE produtos (nome text, qtd integer, preco float)")
+#cursor.execute("CREATE TABLE produtos (nome text, qtd integer, preco float)")
+#cursor.execute("CREATE TABLE produtos ('fone', 12, 350.00)")
+#cursor.execute("CREATE TABLE produtos (monitor, 10, 1150.00)")
+
+banco.commit()
+cursor.execute("INSERT INTO produtos VALUES ('Notebook','3','4500')")
