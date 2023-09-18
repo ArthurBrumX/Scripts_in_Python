7404dc1fbe98b76c44720ab464b7dc43aab1b95d
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-09-18 03:20:10 -0300

    up

---
+++ Classes/celulares.py
@@ -0,0 +1,10 @@
+class celular:
+    def __init__(self,modelo, marca, cor, tamanho, ano_lanc):
+        self.modelo = modelo
+        self.marca = marca
+        self.cor = cor
+        self.tamnho = tamanho
+        self.ano_lanc = ano_lanc
+
+
+c1 = celular("note-9","Xiaomi","azul","8cm","2021")
